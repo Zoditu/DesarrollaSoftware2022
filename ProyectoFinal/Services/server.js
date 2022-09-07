c0aae83c83469d5445a6783e6c9ea9a16460f94a
KMCF <kevin.martin@softtek.com>
2022-09-06 19:01:07 -0500

    Update server name

--- ProyectoFinal/Services/server.js
+++ ProyectoFinal/Services/server.js
@@ -2,7 +2,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const MongoUser = require("C:\\MongoUsers\\user.json");
 
-const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@cluster0.6i37a.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect(err => {
