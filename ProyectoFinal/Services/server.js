dfc051cd5fad424cd73069229850fcb49bb48f5a
KMCF <kevin.martin@softtek.com>
2022-09-09 18:10:37 -0500

    server.js

--- ProyectoFinal/Services/server.js
+++ ProyectoFinal/Services/server.js
@@ -3,6 +3,19 @@ const DB = "DSStore2022";
 const MongoUser = require("../../../MongoUsers/user.json");
 const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/${DB}?retryWrites=true&w=majority`;
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('../../'));
+app.use(express.json());
+
+app.get('/', function(req, res) {
+    res.send({
+        status: "online"
+    });
+});
+
 mongoose.connect(
     uri, {
         useNewUrlParser: true,
@@ -12,14 +25,17 @@ mongoose.connect(
         if (err) {
             console.log(err);
         } else {
-            console.log('Conectado :D');
-            const Cat = mongoose.model('Cat', {
+            console.log('Conectado a la base de datos');
+            app.listen(port, function() {
+                console.log(`Servidor corriendo en http://localhost:${port}`);
+            });
+            /*const Cat = mongoose.model('Cat', {
                 name: String
             });
 
             const kitty = new Cat({
                 name: 'Zildjian'
             });
-            kitty.save().then(() => console.log('meow'));
+            kitty.save().then(() => console.log('meow'));*/
         }
     });
