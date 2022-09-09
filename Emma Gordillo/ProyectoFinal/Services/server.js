9a95673d3d62e242871f1888a3ad052da93623f1
Emma-Gordillo <elizaemma@hotmail.com>
2022-09-08 23:09:06 -0500

    Cambios PF

---
+++ Emma Gordillo/ProyectoFinal/Services/server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const DB = "DSStore2022";
+const MongoUser = require("C:\\MongoUsers\\user.json")
+const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/${DB}?retryWrites=true&w=majority`;
+
+mongoose.connect(
+    uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Conectado');
+            const Cat = mongoose.model('Cat', {
+                name: String
+            });
+
+            const kitty = new Cat({
+                name: 'Zildjian'
+            });
+            kitty.save().then(() => console.log('meow'));
+        }
+    });
+
