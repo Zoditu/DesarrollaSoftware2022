365c80cc299777f37538ad977c8cbb3af59c08ad
KMCF <kevin.martin@softtek.com>
2022-09-20 19:14:14 -0500

    Ejemplo Login

--- ProyectoFinal/sites/home/home.js
+++ ProyectoFinal/sites/home/home.js
@@ -2,18 +2,59 @@ axios({
     method: 'GET',
     url: '/users/profile'
 }).then(function (result) {
-    console.log(result.data);
+    $('#title').html(`Bienvenid@ ${result.data.user.name}... Ya tienes sesión :)`)
+    //console.log(result.data);
 }).catch(function (error) {
     if (error.response) {
         Swal.fire({
             background: 'var(--colors-pink)',
             color: "var(--colors-white)",
             showCloseButton: true,
-            title: 'Login/Register',
-            confirmButtonText: "Login",
-            text: 'Aquí debe venir el formulario del login',
-            footer: '<a class="register-link" href="">No tiene cuenta? Regístrese</a>'
-        });
+            allowOutsideClick: false,
+            title: 'Iniciar Sesión',
+            confirmButtonText: "Iniciar Sesión",
+            confirmButtonColor: 'var(--colors-white)',
+            html: `<section class="row m-0">
+                        <article class="col">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                                <input type="text" id="email" class="form-control" placeholder="Correo" aria-label="Correo"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">**</span>
+                                <input type="password" id="password" class="form-control" placeholder="Contraseña" aria-label="Contraseña"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                        </article>
+                    </section>`,
+            //text: 'Aquí debe venir el formulario del login',
+            footer: '<a class="register-link" href="">No tiene cuenta? Regístrate</a>',
+            showLoaderOnConfirm: true,
+            preConfirm: function (x) {
+                var email = $("#email").val();
+                var password = $("#password").val();
+
+                var payload = {
+                    email: email,
+                    password: password
+                };
+
+                return axios({
+                    method: 'POST',
+                    url: '/users/login',
+                    data: payload
+                }).then(function (result) {
+                    
+                }).catch(function (error) {
+                    Swal.showValidationMessage(`Request failed: ${error}`);
+                });
+            }
+        }).then(function (result) {
+            if(result.value) {
+                window.location.href = window.location.href;
+            }
+        })
     } else {
         console.log(error);
     }
