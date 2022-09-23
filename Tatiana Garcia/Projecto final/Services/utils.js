7f252d44a14b3d05c08b41d91655ff6717e5a6e0
Tatiana Garcia <tatianagarcia0695@hotmail.com>
2022-09-23 11:53:15 -0500

    Actualization
    Home style, home login

--- Tatiana Garcia/Projecto final/Services/utils.js
+++ Tatiana Garcia/Projecto final/Services/utils.js
@@ -1,16 +1,18 @@
 module.exports = {
     generateToken: function(email) {
-        var tokenPart1 = Date.now();//Date now crea un codigo por milisegundo
+        var tokenPart1 = Date.now(); //16121241541254
         var tokenPart2 = '';
         for (var i = 0; i < email.length; i++) {
             var c = email.charCodeAt(i);
             tokenPart2 += c;
         }
+        //454545654684756465456465464564
         
         var tokenPart3 = Date.now();
+        //16121241541255
 
         var token = tokenPart1 + tokenPart2 + tokenPart3;
-        return Buffer.from(token).toString('base64')
+        return Buffer.from(token).toString('base64');
     },
     encodeEmail: function(email) {
         var encodedEmail = '';
@@ -18,6 +20,7 @@ module.exports = {
             var c = email.charCodeAt(i);
             encodedEmail += c;
         }
+
         var tokenPart2 = Date.now();
 
         return Buffer.from(encodedEmail + tokenPart2).toString('base64');
