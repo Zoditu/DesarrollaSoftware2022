88016137e5d960202589083be090f559431e81ca
ElyAlmaguer <elyalmaguer@gmail.com>
2022-09-28 12:30:13 -0500

    proyecto final
    services,, sites,

---
+++ ElyAlmaguer/PoyectoFinal/Services/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model('User', {
+    username: String,
+    email: String,
+    password: String,
+    name: String,
+    lastName: String,
+    phone: String,
+    address: {
+        street: String,
+        no: Number,
+        hood: String,
+        city: String,
+        state: String,
+        country: String,
+        zip: Number,
+        details: String
+    },
+    orders: Array,
+    cartId: String,
+    permissions: {
+        admin: Boolean,
+        regular: Boolean,
+        enabled: Boolean
+    },
+    sessions: Object
+}, 'Users');
