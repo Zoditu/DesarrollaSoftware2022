38972f0fb71733bd7ef41853d350c7eeab6a2d63
Tatiana Garcia <tatianagarcia0695@hotmail.com>
2022-10-10 19:07:00 -0500

    ExReact

--- Tatiana Garcia/Projecto final/sites/ReactEx/Ex6/ex6.jsx
+++ Tatiana Garcia/Projecto final/sites/ReactEx/Ex6/ex6.jsx
@@ -1,89 +1,131 @@
+//Creación del menú con React
 function Menu() {
     return <>
-        <h1>Ejericio 6</h1>
+        <h1>Ejercicio 6</h1>
     </>;
 }
 
-ReactDOM.createRoot(document.getElementById("menu")).render(<Menu/>);
+ReactDOM.createRoot(document.getElementById("menu")).render(<Menu />);
 
+//Creación de una tarjeta de producto
+// Labial, rojo, marca Loreal, 50g, mediano, Modelo X, imagen(es), precio
 /**
- * 
  * @param {Array} products 
  */
-function Products(products) {
+function Products(props) {
 
+    const products = props.products;
     var prod = [];
 
-    for(var i=0; i<products.length; i++){
-        var product = products[i]
+    for(var i = 0; i < products.length; i++) {
+        var product = products[i];
 
         prod.push(<>
-                        <div class="col-xl-3 col-lg-4 pb-3 pr-2">
-                    <div class="card" style="width: 18rem;">
-                        <div id={"carouselExampleControls-Product-" + i} class="carousel slide card-img-top" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="https://sss05.suburbia.com.mx/xl/SB4021467.jpg" class="d-block w-100" alt="..."/>
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00360052374721L.jpg"
-                                        class="d-block w-100" alt="..."/>
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="https://m.media-amazon.com/images/I/61d5kJv+44L.jpg" class="d-block w-100" alt="..."/>
-                                </div>
+            <article className="col-xl-3 col-lg-4 pb-3">
+                <div className="card">
+                    <div id={"Controls-Carrousel-Product-" + i} className="carousel slide card-img-top" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="https://s.cornershopapp.com/product-images/3813403.jpg?versionId=GYrVEUfO9R9HHwCS3RDY1DjZJ4eqNnBX" className="d-block w-100" alt="..." />
                             </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target={"carouselExampleControls-Product-" + i}
-                                data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target={"carouselExampleControls-Product-" + i}
-                                data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Producto - Modelo</h5>
-                            <p class="card-text">Descripcion del producto</p>
-                        </div>
-                        <div class="accordion" id="accordionExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingOne">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        Detalles del producto
-                                    </button>
-                                </h2>
-                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                                    data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
-                                        <ul class="list-group list-group-flush">
-                                            <li class="list-group-item">Nombre</li>
-                                            <li class="list-group-item">Color:</li>
-                                            <li class="list-group-item">Marca: </li>
-                                            <li class="list-group-item">Peso:</li>
-                                            <li class="list-group-item">Modelo:</li>
-                                            <li class="list-group-item">Tamaño:</li>
-                                        </ul>
-                                    </div>
-                                </div>
+                            <div className="carousel-item">
+                                <img src="https://d3ugyf2ht6aenh.cloudfront.net/stores/002/077/166/products/labial_l_ore_al_pari_s_color_riche_ultra_matte_no_cage_tester_1-4e84367afdd53ae68516496980960802-1024-1024.jpg" className="d-block w-100" alt="..." />
+                            </div>
+                            <div className="carousel-item">
+                                <img src="https://www.nailistas.com/wp-content/uploads/2015/06/labiales-color-riche-swatches-review-loreal-02.jpg" className="d-block w-100" alt="..." />
                             </div>
                         </div>
-                        <div class="card-body">
-                            <span class="float-left precio-producto">
-                                199.99
-                            </span>
-                            <button type="button" class="float-rigth agregar-producto">
-                                <span class="material-icons">
-                                    shopping_cart
-                                </span>
-                            </button>
+                        <button className="carousel-control-prev" type="button" data-bs-target={"#Controls-Carrousel-Product-" + i}
+                            data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target={"#Controls-Carrousel-Product-" + i}
+                            data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">{product.name}</h5>
+                        <p className="card-text">{product.description}</p>
+                    </div>
+        
+                    <div className="accordion accordion-flush" id={"Accordeon-" + i}>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id={"Accordeon-Heading-" + i}>
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target={"#Accordeon-Flush-" + i} aria-expanded="false" aria-controls="flush-collapseOne2">
+                                    Detalles del producto
+                                </button>
+                            </h2>
+                            <div id={"Accordeon-Flush-" + i} className="accordion-collapse collapse" aria-labelledby={"Accordeon-Heading-" + i}
+                                data-bs-parent={"#Accordeon-" + i}>
+                                <div className="accordion-body">
+                                    <ul className="list-group list-group-flush">
+                                        <li className="list-group-item">Nombre: {product.name}</li>
+                                        <li className="list-group-item">Modelo: {product.model}</li>
+                                        <li className="list-group-item">Marca: {product.brand}</li>
+                                        <li className="list-group-item">Color: {product.color}</li>
+                                        <li className="list-group-item">Peso: {product.weight}</li>
+                                        <li className="list-group-item">Tamaño: {product.size}</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
+        
+                    <div className="card-body">
+                        <span className="float-left precio-producto">{product.price}</span>
+                        <button type="button" className="btn btn-primary float-right agregar-producto">
+                            <span className="material-icons">
+                                add_shopping_cart
+                            </span>
+                        </button>
+                    </div>
                 </div>
-        </>)
+            </article>
+        </>);
     }
-    ReactDOM.createRoot(document.getElementById("menu")).render(<Menu/>);
+
+    return <>
+        <section className="row">
+            {prod}
+        </section>
+    </>;
 }
+
+const listaProductos = [
+    {
+        name: "Labial",
+        description: "Esta es la descripción del producto!",
+        model: "Pasión",
+        brand: "Lorea'l",
+        color: "Rojo",
+        weight: "45g",
+        size: "Mediano",
+        price: 120.15
+    },
+    {
+        name: "Labial 2",
+        description: "Esta es la descripción del producto!",
+        model: "Pasión",
+        brand: "Lorea'l",
+        color: "Rojo",
+        weight: "45g",
+        size: "Mediano",
+        price: 121.15
+    },
+    {
+        name: "Labial 3",
+        description: "Esta es la descripción del producto!",
+        model: "Pasión",
+        brand: "Lorea'l",
+        color: "Rojo",
+        weight: "45g",
+        size: "Mediano",
+        price: 124.15
+    }
+];
+
+ReactDOM.createRoot(document.getElementById("app")).render(<Products products={listaProductos} />);
