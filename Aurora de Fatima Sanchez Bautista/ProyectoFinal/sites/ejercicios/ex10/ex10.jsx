e5dbd8e63f22e228926d9260431d0629ad0c1053
AuroraDeFatima <aurisfsb@gmail.com>
2022-10-15 01:20:55 -0500

    ejercicios hasta clase 14 de octubre

---
+++ Aurora de Fatima Sanchez Bautista/ProyectoFinal/sites/ejercicios/ex10/ex10.jsx
@@ -0,0 +1,13 @@
+function Texto(props) {
+    var[text, setText] = React.useState(props.text);
+
+    return<>
+    <input type="text" defaultValue={text} onInput={function() {
+        //text = event.target.value;
+        setText(event.target.value);
+    }} />
+    <h1>{text}</h1>
+    </>;
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<Texto tex="Valor inicial"/>);
