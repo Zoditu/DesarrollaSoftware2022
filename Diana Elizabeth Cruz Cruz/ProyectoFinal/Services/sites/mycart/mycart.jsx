fe1c78cd02e6f5af6cca5a6a76694ae5ffa4edd2
diana92cc <108435384+diana92cc@users.noreply.github.com>
2022-11-01 19:05:27 -0600

    actualizacion Diana

---
+++ Diana Elizabeth Cruz Cruz/ProyectoFinal/Services/sites/mycart/mycart.jsx
@@ -0,0 +1,9 @@
+function Cart()
+var [showLoader, setShowLoader] = React.useState(true);
+var [user, setUser] = React.useState(null);
+var [car, setCar] = React.useState({product:[]});
+
+var products = [];
+for (var i = 0; i < cart.product.length; i++){
+    const product = cart.products[i];
+}
