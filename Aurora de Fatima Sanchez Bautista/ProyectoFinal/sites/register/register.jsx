8c89ea9473182b6271ff9499f457bb27910aa6ef
AuroraDeFatima <aurisfsb@gmail.com>
2022-11-10 17:38:40 -0600

    Actualizacion de proyecto hasta 9 de noviembre

--- Aurora de Fatima Sanchez Bautista/ProyectoFinal/sites/register/register.jsx
+++ Aurora de Fatima Sanchez Bautista/ProyectoFinal/sites/register/register.jsx
@@ -22,19 +22,9 @@ function Register() {
         }
     }, []);
 
-    var menu = <>
-        <header className="main-menu sticky-top">
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <a title="Make Up | Home" className="navbar-brand menu-logo me-2" href="/">Make Up</a>
-                </div>
-            </nav>
-        </header>
-    </>;
-
     var html = <>
         <Loader visible={showLoader} />
-        {menu}
+        <SimpleMenu />
         <main className="container p-0">
             <h1>Crear una nueva cuenta</h1>
             <hr />
