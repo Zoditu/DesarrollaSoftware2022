b669ceda6616245ece0b1d699b89fa027113d9cf
Emma-Gordillo <elizaemma@hotmail.com>
2022-11-14 23:19:21 -0600

    Changes

--- Emma Gordillo/ProyectoFinal/Services/server.js
+++ Emma Gordillo/ProyectoFinal/Services/server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB = "EmmaG";
+const DB = "DSStore2022";
 const MongoUser = require("C:\\MongoUsers\\user.json");
 const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/${DB}?retryWrites=true&w=majority`;
 
