65b096ed8bc530a9fe0f06bf739b3004a299abb6
CecyRojas <ing.cecilia.rojas@gmail.com>
2022-11-17 21:48:48 -0600

    Update server.js

--- Cecy Rojas/ProyectoFinal/Services/server.js
+++ Cecy Rojas/ProyectoFinal/Services/server.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const MongoUser = require("C:\\MongoUsers\\user.json")
 
-const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@cluster0.dc05g7j.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 client.connect(err => {
