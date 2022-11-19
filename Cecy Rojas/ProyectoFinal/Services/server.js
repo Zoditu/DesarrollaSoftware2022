ee0b8180e144a4c97479f431716785ebd2e39b7b
CecyRojas <ing.cecilia.rojas@gmail.com>
2022-11-18 21:20:21 -0600

    Update server.js

--- Cecy Rojas/ProyectoFinal/Services/server.js
+++ Cecy Rojas/ProyectoFinal/Services/server.js
@@ -1,16 +1,25 @@
-
 const mongoose = require('mongoose');
 const DB = "Baloc";
 const MongoUser = require("C:\\MongoUsers\\user.json");
 const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/${DB}?retryWrites=true&w=majority`;
 
 mongoose.connect(
-    uri, 
-    { useNewUrlParser: true, useUnifiedTopology: true},
+    uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
     err => {
-        if(err) {
+        if (err) {
             console.log(err);
-        } else{
+        } else {
             console.log('Conectado :)');
-    }
-});
+            const Cat = mongoose.model('Cat', {
+                name: String
+            });
+
+            const kitty = new Cat({
+                name: 'Gatito'
+            });
+            kitty.save().then(() => console.log('meow'));
+        }
+    });
