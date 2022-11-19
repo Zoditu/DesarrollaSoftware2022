56ba8d4d33759d85e3d4c92912618a59ef85487e
CecyRojas <ing.cecilia.rojas@gmail.com>
2022-11-18 20:01:13 -0600

    Update server.js

--- Cecy Rojas/ProyectoFinal/Services/server.js
+++ Cecy Rojas/ProyectoFinal/Services/server.js
@@ -4,4 +4,13 @@ const DB = "Baloc";
 const MongoUser = require("C:\\MongoUsers\\user.json");
 const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/${DB}?retryWrites=true&w=majority`;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+mongoose.connect(
+    uri, 
+    { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 },
+    err => {
+        if(err) {
+            console.log(err);
+        } else{
+            console.log('Conectado :)');
+    }
+});
