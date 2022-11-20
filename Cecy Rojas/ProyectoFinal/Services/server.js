18d4016a51560ad32372598f6658c9604d7eefd9
CecyRojas <ing.cecilia.rojas@gmail.com>
2022-11-19 18:48:59 -0600

    Update server.js

--- Cecy Rojas/ProyectoFinal/Services/server.js
+++ Cecy Rojas/ProyectoFinal/Services/server.js
@@ -3,6 +3,13 @@ const DB = "Baloc";
 const MongoUser = require("C:\\MongoUsers\\user.json");
 const uri = `mongodb+srv://${MongoUser.user}:${MongoUser.password}@${MongoUser.server}/${DB}?retryWrites=true&w=majority`;
 
+const express = require('express');
+const app = (express);
+const port = 3000;
+
+app.use(express.static('../../'));
+app.use(express.json());
+
 mongoose.connect(
     uri, {
         useNewUrlParser: true,
@@ -12,14 +19,17 @@ mongoose.connect(
         if (err) {
             console.log(err);
         } else {
-            console.log('Conectado :)');
-            const Cat = mongoose.model('Cat', {
+            console.log('Conectado a la BD de Baloc');
+            app.listen(port, function () {
+                console.log(`Servidor corriendo en https://localhost:${port}`);
+            }); 
+            /*const Cat = mongoose.model('Cat', {
                 name: String
             });
 
             const kitty = new Cat({
                 name: 'Gatito'
             });
-            kitty.save().then(() => console.log('meow'));
+            kitty.save().then(() => console.log('meow'));*/
         }
     });
